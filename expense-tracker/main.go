db1f9d6ad713a43b7743e567bc8360ba19218b09
adarsh <adarshsingh8853757189@gmail.com>
2025-05-24 15:29:45 +0530

    added updateexpense feature

--- expense-tracker/main.go
+++ expense-tracker/main.go
@@ -68,23 +68,68 @@ func addExpense(description string, amount float64) error {
 	fmt.Printf("Expense added successfully (ID: %d)\n", expense.ID)
 	return nil
 }
+func updateExpense(description string, amount float64, Id int) error {
+	expenses, err := loadExpense()
+	if err != nil {
+		panic(err)
+	}
+	update := false
+	for i, exp := range expenses {
+		if exp.ID == Id {
+			if description != "" {
+				expenses[i].Description = description
+			}
+			if amount < 0 {
+				expenses[i].Amount = amount
+			}
+			update = true
+			break
+		}
+	}
+	if !update {
+		fmt.Println("the Id that you are searching is not available")
+	}
+	err = saveExpenses(expenses)
+	if err != nil {
+		return err
+	}
 
+	fmt.Println("Expense updated successfully")
+	return nil
+}
 func main() {
 	fmt.Println("We are going to develop CLi for expense-tracker")
-	if len(os.Args) < 2 || os.Args[1] != "add" {
-		println("Provide a valid argument")
-	}
-	addflag := flag.NewFlagSet("add", flag.ExitOnError)
-	desc := addflag.String("description", "", "Expense description")
-	amt := addflag.Float64("amount", 0, "Expense amount")
+	if os.Args[1] == "add" {
+		if len(os.Args) < 2 || os.Args[1] != "add" {
+			println("Provide a valid argument")
+		}
+		addflag := flag.NewFlagSet("add", flag.ExitOnError)
+		desc := addflag.String("description", "", "Expense description")
+		amt := addflag.Float64("amount", 0, "Expense amount")
 
-	addflag.Parse(os.Args[2:])
-	if *desc == "" || *amt == 0 {
-		fmt.Println("Both --description and --amount are required")
-		return
+		addflag.Parse(os.Args[2:])
+		if *desc == "" || *amt == 0 {
+			fmt.Println("Both --description and --amount are required")
+			return
+		}
+		err := addExpense(*desc, *amt)
+		if err != nil {
+			fmt.Println("Error adding expense:", err)
+		}
 	}
-	err := addExpense(*desc, *amt)
-	if err != nil {
-		fmt.Println("Error adding expense:", err)
+	if os.Args[1] == "update" {
+		updateflag := flag.NewFlagSet("update", flag.ExitOnError)
+		Id := updateflag.Int("id", 0, "ID to update")
+		newdesc := updateflag.String("description", " ", "New description")
+		newamt := updateflag.Float64("amount", 0, "New amount")
+		updateflag.Parse(os.Args[2:])
+		if *newdesc == "" || *newamt == 0 || *Id < 0 {
+			fmt.Println("Both --description and --amount are required")
+			return
+		}
+		err := updateExpense(*newdesc, *newamt, *Id)
+		if err != nil {
+			fmt.Println("Error in updating the expense", err)
+		}
 	}
 }
